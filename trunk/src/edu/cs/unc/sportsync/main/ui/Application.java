21ce143b850a789024e1b5c0367c30dc89ef7195
Pwaivers@gmail.com <Pwaivers@gmail.com@7fc14471-e47b-f046-278a-3a48cb2caaad>
2012-09-17 01:07:43 +0000

    Changed GUI from a single complex screen to a simple main screen with preferences window available from the settings buttons.  This is the first version of our newest GUI interface.

--- trunk/src/edu/cs/unc/sportsync/main/ui/Application.java
+++ trunk/src/edu/cs/unc/sportsync/main/ui/Application.java
@@ -14,7 +14,6 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.FormAttachment;
 import org.eclipse.swt.layout.FormData;
 import org.eclipse.swt.layout.FormLayout;
-import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.MenuItem;
 
@@ -22,33 +21,29 @@ public class Application {
 
 	private Shell shell;
 	private Shell dialog;
-	private Scale scale;
-	Combo combo1, combo2;
+	private int delayTime;  // in seconds
 
 	public Application(Display display) {
 
-		
-		shell = new Shell(display, SWT.CLOSE | SWT.TITLE | SWT.MIN );
+		shell = new Shell(display, SWT.CLOSE | SWT.TITLE | SWT.MIN);
 		shell.setText("UNC SportsSync");
-		initUI();
 		
-//		optionsShell = new Shell(shell, SWT.TITLE);
-//		optionsShell.setText("Options...");
-//		optionsShell.setSize(300, 300);
-//		optionsShell.setLocation(200, 200);
-//		optionsShell.open();
-//		optionsShell.setVisible(false);
+		delayTime = 60;
 
-		dialog.setSize(250,150);
-		
+		initMainUI();
+
+		// dialog = new Shell(shell, SWT.TITLE);
+		// dialog.setText("Options...");
+		// dialog.setSize(300, 300);
+		// dialog.setLocation(200, 200);
+		// dialog.open();
+		// dialog.setVisible(false);
+
+		// dialog.open();
+
 		shell.setSize(600, 450);
 		shell.setLocation(100, 100);
-
 		shell.open();
-		
-		while (!dialog.isDisposed ()) {
-			if (!display.readAndDispatch ()) display.sleep ();
-		}
 
 		while (!shell.isDisposed()) {
 			if (!display.readAndDispatch()) {
@@ -56,121 +51,84 @@ public class Application {
 			}
 		}
 	}
+	
 
-	public void initUI() {
+	private Scale scale;
+	private Label endScale;
+
+	public void initMainUI() {
 		FormLayout layout = new FormLayout();
 		shell.setLayout(layout);
-		
-		// Define Options Dialog
-		dialog = new Shell (shell);
-		dialog.setLayout (new FormLayout());
-		final Button ok = new Button (dialog, SWT.PUSH);
-		ok.setText ("OK");
-		Button cancel = new Button (dialog, SWT.PUSH);
-		cancel.setText ("Cancel");
-		Listener listener =new Listener () {
-			public void handleEvent (Event event) {
-				dialog.close ();
+		//
+		// // Define Options Dialog
+		// //dialog = new Shell (shell);
+		// dialog.setLayout (new FormLayout());
+		// // Labels for INPUT and OUTPUT
+		// Label dl = new Label(dialog, SWT.LEFT);
+		// dl.setText("Caption");
+		// FormData dld = new FormData(100, 30);
+		// dld.left = new FormAttachment(50);
+		// dld.bottom = new FormAttachment(100);
+		// dl.setLayoutData(dld);
+		// final Button ok = new Button (dialog, SWT.PUSH);
+		// ok.setText ("OK");
+		// Button cancel = new Button (dialog, SWT.PUSH);
+		// cancel.setText ("Cancel");
+		// Listener listener =new Listener () {
+		// public void handleEvent (Event event) {
+		// dialog.close ();
+		// }
+		// };
+		// ok.addListener (SWT.Selection, listener);
+		// cancel.addListener (SWT.Selection, listener);
+
+		// Line-out options button
+		Button settingsButton = new Button(shell, SWT.PUSH);
+		settingsButton.setText("Settings");
+		FormData settingsButtonData = new FormData(60, 25);
+		settingsButtonData.right = new FormAttachment(100);
+		settingsButtonData.top = new FormAttachment(0);
+		settingsButton.setLayoutData(settingsButtonData);
+
+		Listener listener = new Listener() {
+			public void handleEvent(Event event) {
+				initSettingsUI();
+				dialog.open();
 			}
 		};
-		ok.addListener (SWT.Selection, listener);
-		cancel.addListener (SWT.Selection, listener);
-		
-		// INPUT and OUTPUT selection		
-		combo2 = new Combo (shell, SWT.READ_ONLY);
-		combo2.setItems (new String [] {"Line-Out Jack", "Speakers"});
-		combo2.select(0);
-		FormData combo2F = new FormData(100, 100);
-		combo2F.left = new FormAttachment(70);
-		combo2F.bottom = new FormAttachment(85);
-		combo2.setLayoutData(combo2F);
-		
-		combo2.addListener(SWT.Selection, new Listener() {
-			public void handleEvent(Event event) {
-				System.out.println("Input Selection detail -> " + combo2.getText());
-				dialog.pack ();
-				dialog.open ();
-				
-			}
-		});
-		
-		combo1 = new Combo (shell, SWT.READ_ONLY);
-		combo1.setItems (new String [] {"Line-In", "Online Radio"});
-		combo1.select(0);
-		FormData combo1F = new FormData(100, 100);
-		combo1F.left = new FormAttachment(10);
-		combo1F.bottom = new FormAttachment(85);
-		combo1.setLayoutData(combo1F);
-		
-		combo1.addListener(SWT.Selection, new Listener() {
-			public void handleEvent(Event event) {
-				System.out.println("Input Selection detail -> " + combo1.getText());
-			}
-		});
-		
-		// Labels for INPUT and OUTPUT
-		Label ln = new Label(shell, SWT.LEFT);
-		ln.setText("Audio Input:");
-		FormData lnd = new FormData(100, 30);
-		lnd.left = new FormAttachment(combo1, 0, SWT.LEFT);
-		lnd.bottom = new FormAttachment(combo1, -20, SWT.BOTTOM);
-		ln.setLayoutData(lnd);
-		
-		Label lo = new Label(shell, SWT.LEFT);
-		lo.setText("Audio Output:");
-		FormData lod = new FormData(100, 30);
-		lod.left = new FormAttachment(combo2, 0, SWT.LEFT);
-		lod.bottom = new FormAttachment(combo2, -20, SWT.BOTTOM);
-		lo.setLayoutData(lod);
-		
-		//LALALA new comment!
-		
-		// Line-in options button
-		Button inOption = new Button(shell, SWT.PUSH);
-		inOption.setText("Options...");
-		FormData inOptionData = new FormData(60, 25);
-		inOptionData.left = new FormAttachment(combo1, 5, SWT.LEFT);
-		inOptionData.bottom = new FormAttachment(combo1, 40, SWT.BOTTOM);
-		inOption.setLayoutData(inOptionData);
-		
-		// Line-out options button
-		Button outOption = new Button(shell, SWT.PUSH);
-		outOption.setText("Options...");
-		FormData outOptionData = new FormData(60, 25);
-		outOptionData.left = new FormAttachment(combo2, 5, SWT.LEFT);
-		outOptionData.bottom = new FormAttachment(combo2, 40, SWT.BOTTOM);
-		outOption.setLayoutData(outOptionData);
 
-//		Button cancelButton = new Button(shell, SWT.PUSH);
-//		cancelButton.setText("Input");
-//
-//		FormData cancelData = new FormData(80, 30);
-//		cancelData.right = new FormAttachment(90);
-//		cancelData.bottom = new FormAttachment(95);
-//		cancelButton.setLayoutData(cancelData);
-//
-//		Button okButton = new Button(shell, SWT.PUSH);
-//		okButton.setText("OK");
-//		
-//		FormData okData = new FormData(80, 30);
-//		okData.right = new FormAttachment(cancelButton, -5, SWT.LEFT);
-//		okData.bottom = new FormAttachment(cancelButton, 0, SWT.BOTTOM);
-//		okButton.setLayoutData(okData);
-		
-//
-//		Button clearButton = new Button(shell, SWT.PUSH);
-//		clearButton.setText("Clear");
-//
-//		FormData clearData = new FormData(80, 30);
-//		clearData.left = new FormAttachment(10);
-//		clearData.bottom = new FormAttachment(95);
-//		clearButton.setLayoutData(clearData);
+		settingsButton.addListener(SWT.Selection, listener);
 
-//		Label label = new Label(shell, SWT.LEFT);
-//		label.setText("Label 1");
-//		FormData nameLabelData = new FormData(200, 20);
-//		nameLabelData.left = new FormAttachment(10);
-//		nameLabelData.top = new FormAttachment(10);
+		// Button cancelButton = new Button(shell, SWT.PUSH);
+		// cancelButton.setText("Input");
+		//
+		// FormData cancelData = new FormData(80, 30);
+		// cancelData.right = new FormAttachment(90);
+		// cancelData.bottom = new FormAttachment(95);
+		// cancelButton.setLayoutData(cancelData);
+		//
+		// Button okButton = new Button(shell, SWT.PUSH);
+		// okButton.setText("OK");
+		//
+		// FormData okData = new FormData(80, 30);
+		// okData.right = new FormAttachment(cancelButton, -5, SWT.LEFT);
+		// okData.bottom = new FormAttachment(cancelButton, 0, SWT.BOTTOM);
+		// okButton.setLayoutData(okData);
+
+		//
+		// Button clearButton = new Button(shell, SWT.PUSH);
+		// clearButton.setText("Clear");
+		//
+		// FormData clearData = new FormData(80, 30);
+		// clearData.left = new FormAttachment(10);
+		// clearData.bottom = new FormAttachment(95);
+		// clearButton.setLayoutData(clearData);
+
+		// Label label = new Label(shell, SWT.LEFT);
+		// label.setText("Label 1");
+		// FormData nameLabelData = new FormData(200, 20);
+		// nameLabelData.left = new FormAttachment(10);
+		// nameLabelData.top = new FormAttachment(10);
 
 		// Create menu bar
 		Menu menuBar = new Menu(shell, SWT.BAR);
@@ -180,31 +138,31 @@ public class Application {
 		Menu fileMenu = new Menu(shell, SWT.DROP_DOWN);
 		cascadeFileMenu.setMenu(fileMenu);
 
-//		// "Input" sub menu in "File" Menu
-//		MenuItem subMenuInput = new MenuItem(fileMenu, SWT.CASCADE);
-//		subMenuInput.setText("Input");
-//
-//		Menu inputSubmenu = new Menu(shell, SWT.DROP_DOWN);
-//		subMenuInput.setMenu(inputSubmenu);
-//
-//		MenuItem micIn = new MenuItem(inputSubmenu, SWT.PUSH);
-//		micIn.setText("&Microphone Line-In");
-//
-//		MenuItem onlineIn = new MenuItem(inputSubmenu, SWT.PUSH);
-//		onlineIn.setText("&Online Radio");
-//
-//		// "Output" sub menu in "File" Menu
-//		MenuItem subMenuItem = new MenuItem(fileMenu, SWT.CASCADE);
-//		subMenuItem.setText("Output");
-//
-//		Menu submenu = new Menu(shell, SWT.DROP_DOWN);
-//		subMenuItem.setMenu(submenu);
-//
-//		MenuItem jackOut = new MenuItem(submenu, SWT.PUSH);
-//		jackOut.setText("&Audio Line-Out");
-//
-//		MenuItem speakers = new MenuItem(submenu, SWT.PUSH);
-//		speakers.setText("&Built-In Speakers");
+		// // "Input" sub menu in "File" Menu
+		// MenuItem subMenuInput = new MenuItem(fileMenu, SWT.CASCADE);
+		// subMenuInput.setText("Input");
+		//
+		// Menu inputSubmenu = new Menu(shell, SWT.DROP_DOWN);
+		// subMenuInput.setMenu(inputSubmenu);
+		//
+		// MenuItem micIn = new MenuItem(inputSubmenu, SWT.PUSH);
+		// micIn.setText("&Microphone Line-In");
+		//
+		// MenuItem onlineIn = new MenuItem(inputSubmenu, SWT.PUSH);
+		// onlineIn.setText("&Online Radio");
+		//
+		// // "Output" sub menu in "File" Menu
+		// MenuItem subMenuItem = new MenuItem(fileMenu, SWT.CASCADE);
+		// subMenuItem.setText("Output");
+		//
+		// Menu submenu = new Menu(shell, SWT.DROP_DOWN);
+		// subMenuItem.setMenu(submenu);
+		//
+		// MenuItem jackOut = new MenuItem(submenu, SWT.PUSH);
+		// jackOut.setText("&Audio Line-Out");
+		//
+		// MenuItem speakers = new MenuItem(submenu, SWT.PUSH);
+		// speakers.setText("&Built-In Speakers");
 
 		MenuItem exitItem = new MenuItem(fileMenu, SWT.PUSH);
 		exitItem.setText("&Exit");
@@ -219,10 +177,10 @@ public class Application {
 		});
 
 		// Slider
-		scale = new Scale(shell, SWT.HORIZONTAL);  // WHY FINAL???
+		scale = new Scale(shell, SWT.HORIZONTAL); // WHY FINAL???
 		// Rectangle clientArea = shell.getClientArea ();
 		scale.setMaximum(1);
-		scale.setMaximum(600);
+		scale.setMaximum(delayTime*10);
 		scale.setIncrement(1);
 		scale.setPageIncrement(10);
 		FormData sliderBar = new FormData(300, 40);
@@ -240,40 +198,39 @@ public class Application {
 
 		scale.addListener(SWT.Selection, new Listener() {
 			public void handleEvent(Event event) {
-				System.out.println("Scroll detail -> " + scale.getSelection()/10.0);
-				scaleValue.setText("Delay: "+scale.getSelection()/10.0+ " seconds");
+				System.out.println("Scroll detail -> " + scale.getSelection()
+						/ 10.0);
+				scaleValue.setText("Delay: " + scale.getSelection() / 10.0
+						+ " seconds");
 				scaleValue.forceFocus();
 			}
 		});
-		
+
 		// labels for scale
 		Label startScale = new Label(shell, SWT.LEFT);
-		startScale.setText("0 sec");
+		startScale.setText("Live");
 		FormData startScaleData = new FormData(50, 30);
 		startScaleData.left = new FormAttachment(scale, 0, SWT.LEFT);
 		startScaleData.bottom = new FormAttachment(scale, 30, SWT.BOTTOM);
 		startScale.setLayoutData(startScaleData);
-		
-		Label endScale = new Label(shell, SWT.LEFT);
-		endScale.setText("60 sec");
+
+		endScale = new Label(shell, SWT.LEFT);
+		endScale.setText(delayTime + " sec");
 		FormData endScaleData = new FormData(50, 30);
 		endScaleData.right = new FormAttachment(scale, 20, SWT.RIGHT);
 		endScaleData.bottom = new FormAttachment(scale, 30, SWT.BOTTOM);
 		endScale.setLayoutData(endScaleData);
-		
-		
-		
 
 		// Slider slider = new Slider (shell, SWT.HORIZONTAL);
 		// Rectangle clientArea = shell.getClientArea ();
 		// slider.setBounds (clientArea.x + 10, clientArea.y + 10, 200, 32);
-		//    	
+		//
 		// /*FormData sliderBar = new FormData(400, 20);
 		// sliderBar.left = new FormAttachment(30);
 		// sliderBar.top = new FormAttachment(50);
 		// slider.setLayoutData(sliderBar);
 		// */
-		//        
+		//
 		// slider.addListener (SWT.Selection, new Listener () {
 		// public void handleEvent (Event event) {
 		// String string = "SWT.NONE";
@@ -292,5 +249,113 @@ public class Application {
 
 		// Point p = label.computeSize(SWT.DEFAULT, SWT.DEFAULT);
 		// label.setBounds(5, 5, p.x+5, p.y+5);
+	}
+
+	Combo inputList, OutputList, DelayList;
+
+	public void initSettingsUI() {
+
+		//dialog = new Shell(shell, SWT.APPLICATION_MODAL);
+		dialog = new Shell(shell);
+		dialog.setText("Settings");
+		dialog.setSize(400, 500);
+		dialog.setLocation(400, 150);
+
+		FormLayout layout = new FormLayout();
+		dialog.setLayout(layout);
+
+		// INPUT and OUTPUT selection
+		inputList = new Combo(dialog, SWT.READ_ONLY);
+		String[] listOfInputDevices = new String[] { "Line-in", "Online Radio" };
+		inputList.setItems(listOfInputDevices);
+		inputList.select(0);
+		FormData inputListF = new FormData(100, 100);
+		inputListF.left = new FormAttachment(50);
+		inputListF.top = new FormAttachment(20);
+		inputList.setLayoutData(inputListF);
+
+		inputList.addListener(SWT.Selection, new Listener() {
+			public void handleEvent(Event event) {
+				System.out.println("Input Selection detail -> "
+						+ inputList.getText());
+			}
+		});
+
+		OutputList = new Combo(dialog, SWT.READ_ONLY);
+		String[] listOfOutputDevices = new String[] { "Line-Out Jack",
+				"Speakers" };
+		OutputList.setItems(listOfOutputDevices);
+		OutputList.select(0);
+		FormData OutputListF = new FormData(100, 100);
+		OutputListF.left = new FormAttachment(inputList, 0, SWT.LEFT);
+		OutputListF.bottom = new FormAttachment(inputList, 100, SWT.BOTTOM);
+		OutputList.setLayoutData(OutputListF);
+
+		OutputList.addListener(SWT.Selection, new Listener() {
+			public void handleEvent(Event event) {
+				System.out.println("Input Selection detail -> "
+						+ OutputList.getText());
+			}
+		});
+
+		// Labels for INPUT and OUTPUT
+		Label ln = new Label(dialog, SWT.LEFT);
+		ln.setText("Audio Input:");
+		FormData lnd = new FormData(100, 30);
+		lnd.left = new FormAttachment(inputList, 0, SWT.LEFT);
+		lnd.bottom = new FormAttachment(inputList, -20, SWT.BOTTOM);
+		ln.setLayoutData(lnd);
+
+		Label lo = new Label(dialog, SWT.LEFT);
+		lo.setText("Audio Output:");
+		FormData lod = new FormData(100, 30);
+		lod.left = new FormAttachment(OutputList, 0, SWT.LEFT);
+		lod.bottom = new FormAttachment(OutputList, -20, SWT.BOTTOM);
+		lo.setLayoutData(lod);
+		
+		// Optional Delay times
+		DelayList = new Combo(dialog, SWT.READ_ONLY);
+		String[] listOfDelayTimes = new String[40];
+		String time;
+		for (int i = 0 ; i < 40 ; i++ ){
+			time = (i+1)/4==0 ? String.format("%d sec", 15*((i+1)%4)) :
+				(15*((i+1)%4)==0 ? String.format("%d min", (i+1)/4) :
+					String.format("%d min & %d sec", (i+1)/4,  15*((i+1)%4)));
+			listOfDelayTimes[i] =  time;
+		}
+		DelayList.setItems(listOfDelayTimes);
+		DelayList.select(delayTime/15-1);
+		FormData DelayListF = new FormData(100, 100);
+		DelayListF.left = new FormAttachment(inputList, 0, SWT.LEFT);
+		DelayListF.bottom = new FormAttachment(inputList, 200, SWT.BOTTOM);
+		DelayList.setLayoutData(DelayListF);
+
+		DelayList.addListener(SWT.Selection, new Listener() {
+			public void handleEvent(Event event) {
+				System.out.println("Input Selection detail -> "
+						+ DelayList.getText());
+				delayTime = (DelayList.getSelectionIndex()+1)*15;
+				endScale.setText(delayTime + " sec");
+				scale.setMaximum(delayTime*10);
+			}
+		});
+
+		
+		// Okay/Exit Button
+		Button ok = new Button(dialog, SWT.PUSH);
+		ok.setText("OK");
+		FormData okForm = new FormData(50, 30);
+		okForm.bottom = new FormAttachment(95);
+		okForm.left = new FormAttachment(45);
+		ok.setLayoutData(okForm);
+		Listener listenerExit = new Listener() {
+			public void handleEvent(Event event) {
+				dialog.close();
+			}
+		};
+		ok.addListener(SWT.Selection, listenerExit);
+		
+		
+
 	}
 }
