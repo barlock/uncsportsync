50748bb0be4f557b920facb3b065522192e747dd
elf13oy@gmail.com <elf13oy@gmail.com@7fc14471-e47b-f046-278a-3a48cb2caaad>
2012-10-03 05:24:49 +0000

    Basic application layout, though nothing is hooked up

--- src/edu/unc/cs/sportsync/main/SportSync.java
+++ src/edu/unc/cs/sportsync/main/SportSync.java
@@ -6,11 +6,33 @@ public class SportSync {
 
     /**
      * @param args
+     * @throws Exception
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         ApplicationPresenter app = new ApplicationPresenter();
         app.open();
         app.dispose();
-    }
 
+        // Uncomment below and comment above to switch from XWT to our previous
+        // MVP
+
+        // Display display = new Display();
+        // Shell application = new Shell(display);
+        // FillLayout layout = new FillLayout();
+        //
+        // application.setLayout(layout);
+        // application.setSize(400, 180);
+        // application.setLocation(500, 500);
+        // application.setText("UNC SportSync");
+        //
+        // Composite appComposite = new Application(application, SWT.None);
+        //
+        // application.open();
+        //
+        // while (!application.isDisposed()) {
+        // if (!display.readAndDispatch()) {
+        // display.sleep();
+        // }
+        // }
+    }
 }
