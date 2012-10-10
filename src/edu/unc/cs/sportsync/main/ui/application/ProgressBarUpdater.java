7aa693414383fa4459c534f74ae4086e1cb76ed5
elf13oy@gmail.com <elf13oy@gmail.com@7fc14471-e47b-f046-278a-3a48cb2caaad>
2012-10-10 15:17:23 +0000

    commiting this finaly?

---
+++ src/edu/unc/cs/sportsync/main/ui/application/ProgressBarUpdater.java
@@ -0,0 +1,20 @@
+package edu.unc.cs.sportsync.main.ui.application;
+
+import org.eclipse.swt.widgets.ProgressBar;
+
+public class ProgressBarUpdater implements Runnable {
+
+    private final ProgressBar bufferProgressBar;
+    private final int percent;
+
+    public ProgressBarUpdater(ProgressBar bar, int p) {
+        bufferProgressBar = bar;
+        percent = p;
+    }
+
+    @Override
+    public void run() {
+        bufferProgressBar.setSelection(percent);
+    }
+
+}
