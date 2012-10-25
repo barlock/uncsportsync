55109182d87c23d7dbea39b9ad687f03a8f045fd
elf13oy@gmail.com <elf13oy@gmail.com@7fc14471-e47b-f046-278a-3a48cb2caaad>
2012-10-25 14:09:10 +0000

    Enjoy our purty new settings page! OH MAN its pretty!

---
+++ src/edu/unc/cs/sportsync/main/ui/settings/HelpPageTab.java
@@ -0,0 +1,46 @@
+package edu.unc.cs.sportsync.main.ui.settings;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.e4.xwt.DefaultLoadingContext;
+import org.eclipse.e4.xwt.IConstants;
+import org.eclipse.e4.xwt.IXWTLoader;
+import org.eclipse.e4.xwt.XWT;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
+
+public class HelpPageTab extends Composite {
+
+	public HelpPageTab(Composite parent, int style) {
+		super(parent, style);
+		setLayout(new FillLayout());
+		// load XWT
+		String name = HelpPageTab.class.getSimpleName() + IConstants.XWT_EXTENSION_SUFFIX;
+		try {
+			URL url = HelpPageTab.class.getResource(name);
+			Map<String, Object> options = new HashMap<String, Object>();
+			options.put(IXWTLoader.CLASS_PROPERTY, this);
+			options.put(IXWTLoader.CONTAINER_PROPERTY, this);
+			XWT.setLoadingContext(new DefaultLoadingContext(this.getClass().getClassLoader()));
+			XWT.loadWithOptions(url, options);
+		} catch (Throwable e) {
+			throw new Error("Unable to load " + name, e);
+		}
+	}
+
+	public void onHomeLinkClick(Event event) {
+		org.eclipse.swt.program.Program.launch("http://code.google.com/p/sportsync/");
+	}
+
+	public void onDownloadsLinkClick(Event event) {
+		org.eclipse.swt.program.Program.launch("http://code.google.com/p/sportsync/downloads/list/");
+	}
+
+	public void onWikiLinkClick(Event event) {
+		org.eclipse.swt.program.Program.launch("http://code.google.com/p/sportsync/w/list");
+	}
+
+}
