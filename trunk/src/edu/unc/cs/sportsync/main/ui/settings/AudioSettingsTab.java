8fe1cf42fe75b93860b05acb70731ed92deda2de
kartikviper <kartikviper@7fc14471-e47b-f046-278a-3a48cb2caaad>
2012-10-26 22:11:29 +0000

    TestOutput Functionality Implemented

--- trunk/src/edu/unc/cs/sportsync/main/ui/settings/AudioSettingsTab.java
+++ trunk/src/edu/unc/cs/sportsync/main/ui/settings/AudioSettingsTab.java
@@ -17,155 +17,151 @@ import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.ProgressBar;
 import org.eclipse.swt.widgets.Spinner;
 
 import edu.unc.cs.sportsync.main.settings.Settings;
 import edu.unc.cs.sportsync.main.sound.AudioControl;
-import edu.unc.cs.sportsync.main.sound.TestLine;
 
 public class AudioSettingsTab extends Composite {
 
-	private ArrayList<Mixer.Info> inputMixerInfo;
-	private ArrayList<Mixer.Info> outputMixerInfo;
-	private Settings settings;
-	private AudioControl audioControl;
+    private ArrayList<Mixer.Info> inputMixerInfo;
+    private ArrayList<Mixer.Info> outputMixerInfo;
+    private Settings settings;
+    private AudioControl audioControl;
 
-	private final int MAX_DELAY = 120;
+    private final int MAX_DELAY = 120;
 
-	@UI
-	Combo inputDeviceCombo;
+    @UI
+    Combo inputDeviceCombo;
 
-	@UI
-	Combo outputDeviceCombo;
+    @UI
+    Combo outputDeviceCombo;
 
-	@UI
-	Spinner maxDelaySpinner;
+    @UI
+    Spinner maxDelaySpinner;
 
-	@UI
-	ProgressBar inputLevelBar;
+    @UI
+    ProgressBar inputLevelBar;
 
-	@UI
-	Button testButton;
+    @UI
+    Button testButton;
 
-	@UI
-	Button applyButton;
+    @UI
+    Button applyButton;
 
-	public AudioSettingsTab(Composite parent, int style) {
-		super(parent, style);
-		setLayout(new FillLayout());
+    public AudioSettingsTab(Composite parent, int style) {
+        super(parent, style);
+        setLayout(new FillLayout());
 
-		// load XWT
-		String name = AudioSettingsTab.class.getSimpleName() + IConstants.XWT_EXTENSION_SUFFIX;
-		try {
-			URL url = AudioSettingsTab.class.getResource(name);
-			Map<String, Object> options = new HashMap<String, Object>();
-			options.put(IXWTLoader.CLASS_PROPERTY, this);
-			options.put(IXWTLoader.CONTAINER_PROPERTY, this);
-			XWT.setLoadingContext(new DefaultLoadingContext(this.getClass().getClassLoader()));
-			XWT.loadWithOptions(url, options);
-		} catch (Throwable e) {
-			throw new Error("Unable to load " + name, e);
-		}
+        // load XWT
+        String name = AudioSettingsTab.class.getSimpleName() + IConstants.XWT_EXTENSION_SUFFIX;
+        try {
+            URL url = AudioSettingsTab.class.getResource(name);
+            Map<String, Object> options = new HashMap<String, Object>();
+            options.put(IXWTLoader.CLASS_PROPERTY, this);
+            options.put(IXWTLoader.CONTAINER_PROPERTY, this);
+            XWT.setLoadingContext(new DefaultLoadingContext(this.getClass().getClassLoader()));
+            XWT.loadWithOptions(url, options);
+        } catch (Throwable e) {
+            throw new Error("Unable to load " + name, e);
+        }
 
-		updateDeviceLevels();
-	}
+        updateDeviceLevels();
+    }
 
-	private String[] getInputDeviceNames() {
-		/*
-		 * inputMixerInfo = new
-		 * ArrayList<Mixer.Info>(AudioControl.getInputDevices());
-		 */
-		inputMixerInfo = AudioControl.getTargetMixers();
+    private String[] getInputDeviceNames() {
+        /*
+         * inputMixerInfo = new
+         * ArrayList<Mixer.Info>(AudioControl.getInputDevices());
+         */
+        inputMixerInfo = AudioControl.getTargetMixers();
 
-		String[] names = new String[inputMixerInfo.size()];
+        String[] names = new String[inputMixerInfo.size()];
 
-		for (int i = 0; i < inputMixerInfo.size(); i++) {
-			names[i] = inputMixerInfo.get(i).getName();
-		}
+        for (int i = 0; i < inputMixerInfo.size(); i++) {
+            names[i] = inputMixerInfo.get(i).getName();
+        }
 
-		return names;
-	}
+        return names;
+    }
 
-	private String[] getOutputDeviceNames() {
-		/*
-		 * outputMixerInfo = new
-		 * ArrayList<Mixer.Info>(AudioControl.getOutputDevices());
-		 */
-		outputMixerInfo = AudioControl.getSourceMixers();
+    private String[] getOutputDeviceNames() {
+        /*
+         * outputMixerInfo = new
+         * ArrayList<Mixer.Info>(AudioControl.getOutputDevices());
+         */
+        outputMixerInfo = AudioControl.getSourceMixers();
 
-		String[] names = new String[outputMixerInfo.size()];
+        String[] names = new String[outputMixerInfo.size()];
 
-		for (int i = 0; i < outputMixerInfo.size(); i++) {
-			names[i] = outputMixerInfo.get(i).getName();
-		}
+        for (int i = 0; i < outputMixerInfo.size(); i++) {
+            names[i] = outputMixerInfo.get(i).getName();
+        }
 
-		return names;
-	}
+        return names;
+    }
 
-	public boolean hasMaxDelaySpinnerChanged() {
-		return settings.getMaxDelay() != maxDelaySpinner.getSelection();
-	}
+    public boolean hasMaxDelaySpinnerChanged() {
+        return settings.getMaxDelay() != maxDelaySpinner.getSelection();
+    }
 
-	public void setAudioControl(AudioControl control) {
-		audioControl = control;
-	}
+    public void setApplyButtonListener(Listener audioApplyButtonListener) {
+        applyButton.addListener(SWT.Selection, audioApplyButtonListener);
+    }
 
-	public void setSettings(Settings settings) {
+    public void setAudioControl(AudioControl control) {
+        audioControl = control;
+    }
 
-		this.settings = settings;
-		inputDeviceCombo.setItems(getInputDeviceNames()); // DISDAPROBLEM
-		inputDeviceCombo.select(inputMixerInfo.indexOf(settings.getInputMixer()));
-		outputDeviceCombo.setItems(getOutputDeviceNames());
-		outputDeviceCombo.select(outputMixerInfo.indexOf(settings.getOutputMixer()));
+    public void setSettings(Settings settings) {
 
-		maxDelaySpinner.setMaximum(MAX_DELAY);
-		maxDelaySpinner.setSelection(settings.getMaxDelay());
-	}
+        this.settings = settings;
+        inputDeviceCombo.setItems(getInputDeviceNames());
+        inputDeviceCombo.select(inputMixerInfo.indexOf(settings.getInputMixer()));
+        outputDeviceCombo.setItems(getOutputDeviceNames());
+        outputDeviceCombo.select(outputMixerInfo.indexOf(settings.getOutputMixer()));
 
-	public void updateDeviceLevels() {
-		new Thread() {
-			@Override
-			public void run() {
-				while (true) {
-					try {
-						Thread.sleep(60);
-					} catch (Throwable th) {
-					}
-					if (isDisposed()) {
-						return;
-					}
+        maxDelaySpinner.setMaximum(MAX_DELAY);
+        maxDelaySpinner.setSelection(settings.getMaxDelay());
+    }
 
-					getDisplay().asyncExec(new Runnable() {
+    public void setTestButtonListener(Listener audioTestButtonListener) {
+        testButton.addListener(SWT.Selection, audioTestButtonListener);
+    }
 
-						@Override
-						public void run() {
-							if (!isDisposed()) {
-								inputLevelBar.setSelection(audioControl.getInputLevel());
-							}
-						}
+    public void updateDeviceLevels() {
+        new Thread() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        Thread.sleep(60);
+                    } catch (Throwable th) {
+                    }
+                    if (isDisposed()) {
+                        return;
+                    }
 
-					});
-				}
-			}
-		}.start();
-	}
+                    getDisplay().asyncExec(new Runnable() {
 
-	public void updateSettings() {
-		settings.setMaxDelay(maxDelaySpinner.getSelection());
-		settings.setInputMixer(inputMixerInfo.get(inputDeviceCombo.getSelectionIndex()));
-		settings.setOutputMixer(outputMixerInfo.get(outputDeviceCombo.getSelectionIndex()));
-	}
+                        @Override
+                        public void run() {
+                            if (!isDisposed()) {
+                                inputLevelBar.setSelection(audioControl.getInputLevel());
+                            }
+                        }
 
-	public void setApplyButtonListener(Listener audioApplyButtonListener) {
-		applyButton.addListener(SWT.Selection, audioApplyButtonListener);
-	}
+                    });
+                }
+            }
+        }.start();
+    }
 
-	public void onTestButtonSelection(Event event) {
-		TestLine testLine = new TestLine(outputMixerInfo.get(outputDeviceCombo.getSelectionIndex()), audioControl);
-		// getDisplay().asyncExec(testLine);
-		testLine.outputTestWav();
-	}
+    public void updateSettings() {
+        settings.setMaxDelay(maxDelaySpinner.getSelection());
+        settings.setInputMixer(inputMixerInfo.get(inputDeviceCombo.getSelectionIndex()));
+        settings.setOutputMixer(outputMixerInfo.get(outputDeviceCombo.getSelectionIndex()));
+    }
 }
