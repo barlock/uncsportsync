b31be85acfa9f80822d3ce485efdd78b463ba981
kartikviper <kartikviper@7fc14471-e47b-f046-278a-3a48cb2caaad>
2012-10-31 00:00:20 +0000

    Fixed test audio from getting volume of volume slider

--- src/edu/unc/cs/sportsync/main/sound/SoundCheck.java
+++ src/edu/unc/cs/sportsync/main/sound/SoundCheck.java
@@ -1,6 +1,7 @@
 package edu.unc.cs.sportsync.main.sound;
 
-import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.IOException;
 import java.text.DecimalFormat;
 
 import javax.sound.sampled.AudioFormat;
@@ -15,9 +16,9 @@ import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.Mixer;
 import javax.sound.sampled.SourceDataLine;
 import javax.sound.sampled.TargetDataLine;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 import edu.unc.cs.sportsync.main.settings.Settings;
-import edu.unc.cs.sportsync.main.ui.error.ErrorUtil;
 
 public class SoundCheck extends Thread {
 	private final int DELAY_PARAM = 170000;
@@ -33,12 +34,15 @@ public class SoundCheck extends Thread {
 	private boolean fullyCached;
 	private int bufferCacheCount;
 	private int cachingAmount;
+	private int maxDelayAmount;
 
 	private byte[] myBuffer;
 	private byte[] outputBufferQueue;
 
 	private boolean disposed;
 	private final Settings settings;
+	private final File fightSong = new File(
+			"resources//music//UNCFightSong.wav");
 
 	private double percentLevelVolume;
 
@@ -48,13 +52,15 @@ public class SoundCheck extends Thread {
 	/*
 	 * AudioFormat tells the format in which the data is recorded/played
 	 */
-	public SoundCheck(AudioFormat format, int bufferSize, Settings settings, LineListener tAudioListener) throws LineUnavailableException {
+	public SoundCheck(AudioFormat format, int bufferSize, Settings settings,
+			LineListener tAudioListener) throws LineUnavailableException {
 		this.settings = settings;
 		/*
 		 * Get the input/output lines
 		 */
 		BUFFER_SIZE = bufferSize;
 		SOUND_FORMAT = format;
+		maxDelayAmount = settings.getMaxDelay();
 		disposed = false;
 		testAudioListener = tAudioListener;
 
@@ -81,7 +87,8 @@ public class SoundCheck extends Thread {
 	}
 
 	public int getBufferPercentage() {
-		double percent = fullyCached ? 100 : ((double) bufferCacheCount / cachingAmount) * 100;
+		double percent = fullyCached ? 100
+				: ((double) bufferCacheCount / cachingAmount) * 100;
 		return (int) percent;
 	}
 
@@ -90,7 +97,7 @@ public class SoundCheck extends Thread {
 	}
 
 	public int getMaxDelay() {
-		return settings.getMaxDelay();
+		return maxDelayAmount;
 	}
 
 	public double getOutputLevel() {
@@ -118,7 +125,8 @@ public class SoundCheck extends Thread {
 		Line.Info[] targetlineinfos = inputMixer.getTargetLineInfo();
 		for (int i = 0; i < targetlineinfos.length; i++) {
 			if (targetlineinfos[i].getLineClass() == TargetDataLine.class) {
-				inputLine = (TargetDataLine) AudioSystem.getLine(targetlineinfos[i]);
+				inputLine = (TargetDataLine) AudioSystem
+						.getLine(targetlineinfos[i]);
 				break;
 			}
 		}
@@ -126,7 +134,8 @@ public class SoundCheck extends Thread {
 		Line.Info[] sourcelineinfos = outputMixer.getSourceLineInfo();
 		for (int i = 0; i < sourcelineinfos.length; i++) {
 			if (sourcelineinfos[i].getLineClass() == SourceDataLine.class) {
-				outputLine = (SourceDataLine) AudioSystem.getLine(sourcelineinfos[i]);
+				outputLine = (SourceDataLine) AudioSystem
+						.getLine(sourcelineinfos[i]);
 				break;
 			}
 		}
@@ -150,24 +159,25 @@ public class SoundCheck extends Thread {
 		Mixer.Info mixer = settings.getOutputMixer();
 
 		try {
-			testFileInputStream = AudioSystem.getAudioInputStream(new BufferedInputStream(this.getClass().getResourceAsStream("UNCFightSong.wav")));
+			testFileInputStream = AudioSystem.getAudioInputStream(fightSong);
 			myClip = AudioSystem.getClip(mixer);
 			myClip.open(testFileInputStream);
-		} catch (Exception e) {
-			try {
-				testFileInputStream = AudioSystem.getAudioInputStream(new BufferedInputStream(this.getClass().getResourceAsStream("test.wav")));
-				myClip = AudioSystem.getClip(mixer);
-				myClip.open(testFileInputStream);
-			} catch (Exception e1) {
-				e1.printStackTrace();
-				ErrorUtil.openStackTraceDialog("A Fatal Error has occured and the application will need to shut down", e);
-				System.exit(1);
-			}
+		} catch (LineUnavailableException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (UnsupportedAudioFileException e) {
+			e.printStackTrace();
 		}
 
 		myClip.addLineListener(testAudioListener);
 		myClip.start();
-		setVolume();
+		if (myClip.isControlSupported(FloatControl.Type.MASTER_GAIN)) {
+			FloatControl volume = (FloatControl) myClip
+					.getControl(FloatControl.Type.MASTER_GAIN);
+			float maximum = volume.getMaximum();
+			volume.setValue(maximum);
+		}
 	}
 
 	private synchronized int read(byte[] buffer, int offset, int length) {
@@ -175,7 +185,8 @@ public class SoundCheck extends Thread {
 	}
 
 	public synchronized void resetBuffer() {
-		cachingAmount = (int) (Math.ceil((float) DELAY_PARAM / BUFFER_SIZE) * settings.getMaxDelay() + 1);
+		cachingAmount = (int) (Math.ceil((float) DELAY_PARAM / BUFFER_SIZE)
+				* maxDelayAmount + 1);
 		outputBufferQueue = new byte[cachingAmount * BUFFER_SIZE];
 		bufferCacheCount = 0;
 		fullyCached = false;
@@ -198,7 +209,8 @@ public class SoundCheck extends Thread {
 
 			// write to output line
 			// outputBufferQueue[count] = myBuffer.clone();
-			System.arraycopy(myBuffer, 0, outputBufferQueue, bufferCacheCount * BUFFER_SIZE, BUFFER_SIZE);
+			System.arraycopy(myBuffer, 0, outputBufferQueue, bufferCacheCount
+					* BUFFER_SIZE, BUFFER_SIZE);
 
 			if (bufferCacheCount == cachingAmount - 1) {
 				fullyCached = true;
@@ -209,7 +221,8 @@ public class SoundCheck extends Thread {
 			delayVar = (delayAmount * DELAY_PARAM) / 10;
 
 			if (fullyCached || delayVar < (bufferCacheCount - 1) * BUFFER_SIZE) {
-				offset = ((bufferCacheCount + cachingAmount - 1) * BUFFER_SIZE - delayVar) % (cachingAmount * BUFFER_SIZE);
+				offset = ((bufferCacheCount + cachingAmount - 1) * BUFFER_SIZE - delayVar)
+						% (cachingAmount * BUFFER_SIZE);
 
 				// System.out.printf("count = %d offset = %d cachingAmount = %d delayVar = %d bufferSize = %d outputBufferSize = %d\n",
 				// bufferCacheCount, offset, cachingAmount, delayVar,
@@ -217,11 +230,14 @@ public class SoundCheck extends Thread {
 				// outputBufferQueue.length);
 
 				if ((BUFFER_SIZE * cachingAmount - offset) < BUFFER_SIZE) {
-					write(outputBufferQueue, offset, (BUFFER_SIZE * cachingAmount - offset));
-					write(outputBufferQueue, 0, BUFFER_SIZE - (BUFFER_SIZE * cachingAmount - offset));
+					write(outputBufferQueue, offset, (BUFFER_SIZE
+							* cachingAmount - offset));
+					write(outputBufferQueue, 0, BUFFER_SIZE
+							- (BUFFER_SIZE * cachingAmount - offset));
 				} else {
 					write(outputBufferQueue, offset, BUFFER_SIZE);
 				}
+
 			}
 		}
 	}
@@ -232,37 +248,29 @@ public class SoundCheck extends Thread {
 
 	public void setVolume() {
 		if (outputLine.isControlSupported(FloatControl.Type.MASTER_GAIN)) {
-			FloatControl volume = (FloatControl) outputLine.getControl(FloatControl.Type.MASTER_GAIN);
+			FloatControl volume = (FloatControl) outputLine
+					.getControl(FloatControl.Type.MASTER_GAIN);
 			float minimum = volume.getMinimum();
 			float maximum = volume.getMaximum();
-			float currentVolume = (float) (minimum + percentLevelVolume * (maximum - minimum) / 100.0F);
+			float currentVolume = (float) (minimum + percentLevelVolume
+					* (maximum - minimum) / 100.0F);
 			volume.setValue(currentVolume);
 		}
-		if (myClip != null && myClip.isOpen() && myClip.isControlSupported(FloatControl.Type.MASTER_GAIN)) {
-			FloatControl volume = (FloatControl) myClip.getControl(FloatControl.Type.MASTER_GAIN);
-			float minimum = volume.getMinimum();
-			float maximum = volume.getMaximum();
-			float currentVolume = (float) (minimum + percentLevelVolume * (maximum - minimum) / 100.0F);
-			volume.setValue(currentVolume);
-		}
+
 	}
 
 	public void setVolume(double percentLevel) {
 		if (outputLine.isControlSupported(FloatControl.Type.MASTER_GAIN)) {
 			percentLevelVolume = percentLevel;
-			FloatControl volume = (FloatControl) outputLine.getControl(FloatControl.Type.MASTER_GAIN);
+			FloatControl volume = (FloatControl) outputLine
+					.getControl(FloatControl.Type.MASTER_GAIN);
 			float minimum = volume.getMinimum();
 			float maximum = volume.getMaximum();
-			float currentVolume = (float) (minimum + percentLevelVolume * (maximum - minimum) / 100.0F);
+			float currentVolume = (float) (minimum + percentLevelVolume
+					* (maximum - minimum) / 100.0F);
 			volume.setValue(currentVolume);
 		}
-		if (myClip != null && myClip.isOpen() && myClip.isControlSupported(FloatControl.Type.MASTER_GAIN)) {
-			FloatControl volume = (FloatControl) myClip.getControl(FloatControl.Type.MASTER_GAIN);
-			float minimum = volume.getMinimum();
-			float maximum = volume.getMaximum();
-			float currentVolume = (float) (minimum + percentLevelVolume * (maximum - minimum) / 100.0F);
-			volume.setValue(currentVolume);
-		}
+
 	}
 
 	@Override
@@ -274,11 +282,24 @@ public class SoundCheck extends Thread {
 		myClip.stop();
 	}
 
-	public synchronized void toggleMute() {
-		BooleanControl bc = (BooleanControl) outputLine.getControl(BooleanControl.Type.MUTE);
+	public void toggleMute() {
+		BooleanControl bc = (BooleanControl) outputLine
+				.getControl(BooleanControl.Type.MUTE);
 		if (bc != null) {
 			bc.setValue(!bc.getValue());
 		}
+	}
+
+	public void setMute(boolean state) {
+		BooleanControl bc = (BooleanControl) outputLine
+				.getControl(BooleanControl.Type.MUTE);
+		if (bc != null) {
+			bc.setValue(state);
+		}
+	}
+
+	public void updateMaxDelay() {
+		maxDelayAmount = settings.getMaxDelay();
 	}
 
 	// possibly synchronized?
